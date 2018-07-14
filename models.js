f1e5761ada7cef5cd4ea09840c1c95c80aeb7102
mikle9997 <mikle9997@mail.ru>
2018-07-14 20:29:35 +0300

    init

---
+++ models.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+mongoose.model('Document', {
+  properties: ['title', 'data', 'tags'],
+
+  indexes: [
+    'title'
+  ]
+});
+
+exports.Document = function(db) {
+  return db.model('Document');
+};
