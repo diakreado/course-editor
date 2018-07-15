c0e6fd6f0f694ea0d911db74b55ca8e8e0a6c2cb
mikle9997 <mikle9997@mail.ru>
2018-07-15 22:50:15 +0300

    organized adding new names

--- routes/create.js
+++ routes/create.js
@@ -2,14 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 var config = require('../config.js');
+var index = require('./index.js');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('create', { title: config.name_of_project });
 });
 
-router.post('/', function(req, res) {
-    console.log(req.body);
+router.post('/', function (req, res) {
+  index.items.push(req.body['name-of-project']);
+  res.redirect('/');
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,18 @@ var router = express.Router();
 
 var config = require('../config.js');
 
+var array_of_items = [];
+
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: config.name_of_project });
+router.get('/', function (req, res) {
+  console.log(array_of_items);
+  res.render('index', {
+    title: config.name_of_project,
+    items: array_of_items
+  });
 });
 
 
 module.exports = router;
+module.exports.items = array_of_items;
+
